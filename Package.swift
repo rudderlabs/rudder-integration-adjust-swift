6122917b0452fa8aa1e37acf55d8fe11b688c2cf
Pallab Maiti <mynamepallabmaiti@gmail.com>
2023-06-12 14:12:47 +0530

    feat: added SPM support

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/adjust/ios_sdk", "4.29.7"..<"4.29.8"),
-        .package(url: "https://github.com/rudderlabs/rudder-sdk-ios", from: "2.0.0"),
+        .package(name: "Adjust", url: "https://github.com/adjust/ios_sdk", "4.29.7"..<"4.29.8"),
+        .package(name: "Rudder", url: "https://github.com/rudderlabs/rudder-sdk-ios", from: "2.0.0"),
     ],
     targets: [
         .target(
             name: "RudderAdjust",
             dependencies: [
-                .product(name: "Adjust", package: "ios_sdk"),
-                .product(name: "Rudder", package: "rudder-sdk-ios"),
+                .product(name: "Adjust", package: "Adjust"),
+                .product(name: "Rudder", package: "Rudder"),
             ],
             path: "Sources",
             sources: ["Classes/"]
